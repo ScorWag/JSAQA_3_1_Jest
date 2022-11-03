fc5dd761b796c4766173b675929028346e17611b
ScorWag <scorwag@rambler.ru>
2022-11-03 17:19:23 +0300

    Init commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,26 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+  it("Books names should not be sorted", () => {
+    expect(
+      sorting.sortByName([
+        "Властелин Колец",
+        "Властелин Колец",
+        "Властелин Колец",
+      ])
+    ).toEqual(["Властелин Колец", "Властелин Колец", "Властелин Колец"]);
+  });
+});
